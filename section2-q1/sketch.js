c599eef11bcff71537bc5e3b6d4ac50cc6b0b0d3
TomoyaAsada <1990608h@stu.kobe-u.ac.jp>
2020-10-20 13:19:59 +0900

    2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,5 +2,11 @@
 function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    if(i <= 5){
+      stroke(255,0,0);
+    } else if(i >= 6) {
+      stroke(0,0,225);
+    }
+    ellipse(50,50,50 - i * 5);
   }
 }
