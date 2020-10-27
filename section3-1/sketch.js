63871e8c100ffa34081eb2f11ad88c46a85ced29
TomoyaAsada <1990608h@stu.kobe-u.ac.jp>
2020-10-27 12:12:59 +0900

    Update3-1

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -7,6 +7,17 @@ function setup(){
     let theta = TWO_PI * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    star(x, y, 10);
   }
+}
+// 星の関数を組み合わせる
+function star(cx, cy, r){
+  beginShape();
+  for(var i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
