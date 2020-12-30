dfa52530836060c95862038b207edf6b7f933183
TomoyaAsada <1990608h@stu.kobe-u.ac.jp>
2020-12-31 00:06:01 +0900

    3-q1 upDate

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,28 +4,18 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",200,200,0,0,0);
 }
 
-function balloon(t){
+function balloon(t,x,y,a,b,c){
   let w = textWidth(t); //幅
   let h = textAscent() + textDescent();　//高さ
-  let p = 2;
-  fill(0);
-  let x = w/2
-  let y = h
-  ellipse(x,y,w + p * 2,h + p * 2);
+  let p = 30;
+  fill(a,b,c);
+  ellipse(x,y,w + p * 2, h + p * 2);
+  fill(a,b,c);
+  triangle(230,230,260, 190, 280, 250);
 
-//  fill(0)
-//  for(let i = 0; i < 50; i++){
-//   const theta = TWO_PI * i / 50; // TWO_PI は円周率πの2倍（ほかに PI, HALF_PI, QUARTER_PI　がある）
-//   const x = w/2 + cos(theta) * 10; // 関数 cos の戻り値を使用
-//   const y = h + sin(theta) * 10; // 関数 sin の戻り値を使用
-//   ellipse(x, y, w, p);
-// }// 関数 ellipse の副作用で円が描画される
-
-//  fill(0);
-//  rect(0, 0, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, 140, 208);
 }
